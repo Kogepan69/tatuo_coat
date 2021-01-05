05698fc5e91040932c9eb89b0b3531e05744268b
Kogepan69 <kunimiryusei69@icloud.com>
2021-01-05 21:35:21 +0900

    'main'

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     path('price/', views.PriceView.as_view(), name='price'),
     path('contact/', views.ContactView.as_view(), name='contact'),
     path('blog/', views.BlogView.as_view(), name='blog'),
+    path('reservation/', views.ReservationView.as_view(), name='reservation'),
+    path('wash/', views.WashView.as_view(), name='wash'),
+    path('coating/', views.CoatingView.as_view(), name='coating'),
+    path('polishing/', views.PolishingView.as_view(), name='polishing'),
+    path('diagnosis/', views.DiagnosisView.as_view(), name='diagnosis'),
+    path('program/', views.ProgramView.as_view(), name='program'),
+    path('campany/', views.CampanyView.as_view(), name='campany'),
 ]
--- app/views.py
+++ app/views.py
@@ -22,3 +22,31 @@ class ContactView(View):
 class BlogView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'app/blog.html')
+
+class ReservationView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/reservation.html')
+
+class WashView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/wash.html')
+
+class CoatingView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/coating.html')
+
+class PolishingView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/polishing.html')
+
+class DiagnosisView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/diagnosis.html')
+
+class ProgramView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/program.html')
+
+class CampanyView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/campany.html')
