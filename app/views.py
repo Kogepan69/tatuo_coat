e7f93de59cde55f1c25d48291b1da4d2a327e722
Kogepan69 <kunimiryusei69@icloud.com>
2021-01-29 13:50:32 +0900

    'main'

--- app/views.py
+++ app/views.py
@@ -225,7 +225,7 @@ class StaffView(View):
 
 class CalendarView(View):
     def get(self, request, *args, **kwargs):
-        staff_data = Staff.objects.filter(id=self.kwargs['pk']).select_related('user').select_related('store')[0]
+        # staff_data = Staff.objects.filter(id=self.kwargs['pk']).select_related('user').select_related('store')[0]
         today = date.today()
         year = self.kwargs.get('year')
         month = self.kwargs.get('month')
@@ -249,7 +249,7 @@ class CalendarView(View):
             calendar[hour] = row
         start_time = make_aware(datetime.combine(start_day, time(hour=10, minute=0, second=0)))
         end_time = make_aware(datetime.combine(end_day, time(hour=20, minute=0, second=0)))
-        booking_data = Booking.objects.filter(staff=staff_data).exclude(Q(start__gt=end_time) | Q(end__lt=start_time))
+        booking_data = Booking.objects.all().exclude(Q(start__gt=end_time) | Q(end__lt=start_time))
         for booking in booking_data:
             local_time = localtime(booking.start)
             booking_date = local_time.date()
@@ -258,7 +258,7 @@ class CalendarView(View):
                 calendar[booking_hour][booking_date] = False
 
         return render(request, 'app/calendar.html', {
-            'staff_data': staff_data,
+            # 'staff_data': staff_data,
             'calendar': calendar,
             'days': days,
             'start_day': start_day,
@@ -270,7 +270,7 @@ class CalendarView(View):
 
 class BookingView(View):
     def get(self, request, *args, **kwargs):
-        staff_data = Staff.objects.filter(id=self.kwargs['pk']).select_related('user').select_related('store')[0]
+        # staff_data = Staff.objects.filter(id=self.kwargs['pk']).select_related('user').select_related('store')[0]
         year = self.kwargs.get('year')
         month = self.kwargs.get('month')
         day = self.kwargs.get('day')
@@ -278,7 +278,7 @@ class BookingView(View):
         form = BookingForm(request.POST or None)
 
         return render(request, 'app/booking.html', {
-            'staff_data': staff_data,
+            # 'staff_data': staff_data,
             'year': year,
             'month': month,
             'day': day,
@@ -287,33 +287,32 @@ class BookingView(View):
         })
 
     def post(self, request, *args, **kwargs):
-        staff_data = get_object_or_404(Staff, id=self.kwargs['pk'])
+        # staff_data = get_object_or_404(Staff, id=self.kwargs['pk'])
         year = self.kwargs.get('year')
         month = self.kwargs.get('month')
         day = self.kwargs.get('day')
         hour = self.kwargs.get('hour')
         start_time = make_aware(datetime(year=year, month=month, day=day, hour=hour))
         end_time = make_aware(datetime(year=year, month=month, day=day, hour=hour + 1))
-        booking_data = Booking.objects.filter(staff=staff_data, start=start_time)
+        booking_data = Booking.objects.filter(start=start_time)
         form = BookingForm(request.POST or None)
         if booking_data.exists():
             form.add_error(None, '既に予約があります。\n別の日時で予約をお願いします。')
         else:
             if form.is_valid():
                 booking = Booking()
-                booking.staff = staff_data
+                # booking.staff = staff_data
                 booking.start = start_time
                 booking.end = end_time
                 booking.first_name = form.cleaned_data['first_name']
                 booking.last_name = form.cleaned_data['last_name']
                 booking.tel = form.cleaned_data['tel']
-                booking.address = form.cleaned_data['address']
                 booking.remarks = form.cleaned_data['remarks']
                 booking.save()
                 return redirect('thanks')
 
         return render(request, 'app/booking.html', {
-            'staff_data': staff_data,
+            # 'staff_data': staff_data,
             'year': year,
             'month': month,
             'day': day,
